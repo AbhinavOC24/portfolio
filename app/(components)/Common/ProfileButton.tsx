36776cf47506f54ced034bcd6267b9a3c48c7ec4
Abhinav Chauhan <chauhanabhinav822@gmail.com>
2025-08-29 22:09:49 +0530

    Implement code changes to enhance functionality and improve performance

--- app/(components)/Common/ProfileButton.tsx
+++ app/(components)/Common/ProfileButton.tsx
@@ -7,27 +7,27 @@ function ProfileButton() {
   const { color } = getISTStatus();
 
   return (
-    <div className="rounded-[19px] pl-[18px] pt-[18px] w-full h-[124px] my-shadow flex">
-      <div className="w-[88px] h-[82px] rounded-[19px] relative ">
-        <Image
-          src={Pfp}
-          alt="profile"
-          fill
-          className="object-cover object-top rounded-[19px]"
-        ></Image>
-        <div className="absolute bottom-0 right-0 h-[19px] w-[19px] bg-[#F9F3FB] rounded-full  z-10">
-          <div
-            className={`h-[14px] w-[14px] m-[3px] ${color} rounded-full`}
-          ></div>
+    <div className="rounded-xl pl-5 pt-5 w-full h-32 my-shadow flex flex-col z-10">
+      <div className="flex">
+        <div className="w-20 h-20 rounded-[19px] relative ">
+          <Image
+            src={Pfp}
+            alt="profile"
+            fill
+            className="object-cover object-top rounded-2xl"
+          ></Image>
+          <div className="absolute bottom-0 right-0 h-5 w-5 bg-[#F9F3FB] rounded-full  z-10">
+            <div className={`h-3.5 w-3.5 m-1 ${color} rounded-full`}></div>
+          </div>
         </div>
-      </div>
 
-      <div className="flex flex-col ml-[13px]  h-auto mt-[12px]">
-        <div className="text-[26px]  h-auto font-family-neue  font-medium">
-          Abhinav Chauhan
-        </div>
-        <div className="w-auto text-[18px] h-auto translate-y-[-2px]  font-family-pp font-bold text-[#888888]">
-          Available between 9:00 - 20:00 IST ⛈
+        <div className="flex flex-col ml-3  h-auto mt-3">
+          <div className="text-2xl  h-auto font-family-neue  font-medium">
+            Abhinav Chauhan
+          </div>
+          <div className="w-auto text-lg h-auto translate-y-[-2px]  font-family-pp font-bold text-[#888888]">
+            Available between 9:00 - 20:00 IST ⛈
+          </div>
         </div>
       </div>
     </div>
